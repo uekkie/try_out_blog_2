f4060f0cac068b66ef1ba81067ea62235c96999e
Hiroshi Ueki <hirocueki@gmail.com>
2020-02-25 22:28:42 +0900

    Bootstrap, simple_formセットアップ

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy!
+    redirect_to posts_url
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
