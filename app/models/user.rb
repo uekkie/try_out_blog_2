9c0be2a7ffd5722d98112cc50f6ba0f16c1e9ff3
Hiroshi Ueki <hirocueki@gmail.com>
2020-02-26 12:07:24 +0900

    モデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+
+  validates :name, presense: true, uniquiness: true, length: { maximum: 20 }
+
 end
