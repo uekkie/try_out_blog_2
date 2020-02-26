980ad62ad1eb61bafd92cca39a2fb75e63ea0ffd
Hiroshi Ueki <hirocueki@gmail.com>
2020-02-26 17:46:43 +0900

    記法を統一

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   }
   root to: "posts#index"
 
-  resources :posts, only: [:index]
+  resources :posts, only: %i[index]
   
   resources :users, only: %i[edit update] do
     resources :posts
