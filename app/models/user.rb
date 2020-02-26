d047525000740ce84a16c5379e84f076aecd530a
Hiroshi Ueki <hirocueki@gmail.com>
2020-02-26 17:12:34 +0900

    ユーザー名はアルファベットのみ（半角SPは許可しない）

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }, format: { with: /\A[a-zA-Z]+\Z/ }
 
   def email_required?
     false
