df7187459a564a972f73cd5475cf9d1bfd9b9caa
Hiroshi Ueki <hirocueki@gmail.com>
2020-02-28 16:36:55 +0900

    counter_cacheをつかっていいねの数を表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_055132) do
+ActiveRecord::Schema.define(version: 2020_02_28_073202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_055132) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.integer "likes_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
