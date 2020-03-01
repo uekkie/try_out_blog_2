eeb852523ebbdefeb591628fdca202a858584008
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-01 15:24:40 +0900

    使用していない画像フォーマットを削除

--- app/uploaders/post_uploader.rb
+++ app/uploaders/post_uploader.rb
@@ -4,10 +4,6 @@ class PostUploader < CarrierWave::Uploader::Base
   process :convert => 'png'
   process :tags => ['post_image']
 
-  version :standard do
-    process :resize_to_fill => [100, 150, :north]
-  end
-
   version :thumbnail do
     process :resize_to_fit => [300, nil]
   end
