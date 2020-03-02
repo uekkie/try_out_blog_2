0535fb5c3e97d6ef920a6b8c8f81f4cf1cfd4616
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-02 18:57:14 +0900

    Sendgridの送信方式をsmtpに切り替え

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,25 +2,25 @@ if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
 
   #if you are using the API key
-  ActionMailer::Base.smtp_settings = {
-      domain: 'smtp.sendgrid.net',
-      enable_starttls_auto: true,
-      address: "smtp.sendgrid.net",
-      port: 587,
-      authentication: :plain,
-      user_name: "apikey",
-      password: ENV['SENDGRID_API_KEY']
-  }
+  # ActionMailer::Base.smtp_settings = {
+  #     domain: 'smtp.sendgrid.net',
+  #     enable_starttls_auto: true,
+  #     address: "smtp.sendgrid.net",
+  #     port: 587,
+  #     authentication: :plain,
+  #     user_name: "apikey",
+  #     password: ENV['SENDGRID_API_KEY']
+  # }
 #
-#ActionMailer::Base.smtp_settings = {
-#  domain: 'smtp.sendgrid.net',
-#  enable_starttls_auto: true,
-#  address: "smtp.sendgrid.net",
-#  port: 587,
-#  authentication: :login,
-#  user_name: ENV['SENDGRID_USERNAME'],
-#  password: ENV['SENDGRID_PASSWORD'],
-#}
+ActionMailer::Base.smtp_settings = {
+ domain: 'smtp.sendgrid.net',
+ enable_starttls_auto: true,
+ address: "smtp.sendgrid.net",
+ port: 587,
+ authentication: :login,
+ user_name: ENV['SENDGRID_USERNAME'],
+ password: ENV['SENDGRID_PASSWORD'],
+}
 
 elsif Rails.env.development?
   ActionMailer::Base.perform_caching = true
