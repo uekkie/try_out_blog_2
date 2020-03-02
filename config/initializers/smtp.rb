e6907f077acd6ef538be2c08734603001fe35b99
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-02 19:24:13 +0900

    production環境の設定修正

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,6 +1,8 @@
 if Rails.env.production?
-  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.add_delivery_method :sendgrid, Mail::SendGrid, api_key: ENV['SENDGRID_API_KEY']
+  ActionMailer::Base.delivery_method = :sendgrid
 
+  # ActionMailer::Base.delivery_method = :smtp
   #if you are using the API key
   # ActionMailer::Base.smtp_settings = {
   #     domain: 'smtp.sendgrid.net',
@@ -12,15 +14,15 @@ if Rails.env.production?
   #     password: ENV['SENDGRID_API_KEY']
   # }
 #
-ActionMailer::Base.smtp_settings = {
- domain: 'smtp.sendgrid.net',
- enable_starttls_auto: true,
- address: "smtp.sendgrid.net",
- port: 587,
- authentication: :login,
- user_name: ENV['SENDGRID_USERNAME'],
- password: ENV['SENDGRID_PASSWORD'],
-}
+# ActionMailer::Base.smtp_settings = {
+#  domain: 'smtp.sendgrid.net',
+#  enable_starttls_auto: true,
+#  address: "smtp.sendgrid.net",
+#  port: 587,
+#  authentication: :login,
+#  user_name: ENV['SENDGRID_USERNAME'],
+#  password: ENV['SENDGRID_PASSWORD'],
+# }
 
 elsif Rails.env.development?
   ActionMailer::Base.add_delivery_method :sendgrid, Mail::SendGrid, api_key: ENV['SENDGRID_API_KEY']
