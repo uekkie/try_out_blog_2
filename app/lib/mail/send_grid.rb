3a1045673dbd7d11e10376cc77dac54ee4cfcd03
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-03 07:41:45 +0900

    Mailerでメールが飛ばない問題の調査変更分

--- app/lib/mail/send_grid.rb
+++ app/lib/mail/send_grid.rb
@@ -7,11 +7,15 @@ class Mail::SendGrid
     from = SendGrid::Email.new(email: mail.from.first)
     to = SendGrid::Email.new(email: mail.to.first)
     subject = mail.subject
-    content = SendGrid::Content.new(type: 'text/plain', value: mail.body.raw_source)
-    sg_mail = SendGrid::Mail.new(from, subject, to, content)
+    #content = SendGrid::Content.new(type: 'text/plain', value: mail.text_part.body.raw_source)
+    #content = SendGrid::Content.new(type: 'text/html', value: mail.html_part.body.raw_source)
+    sg_mail = SendGrid::Mail.new(from, subject, to)
+    #sg_mail.add_content(Content.new(type: 'text/plain', value: mail.text_part.body.raw_source))
+    sg_mail.add_content(Content.new(type: 'text/html', value: mail.html_part.body.raw_source))
 
     sg = SendGrid::API.new(api_key: @settings[:api_key])
     response = sg.client.mail._('send').post(request_body: sg_mail.to_json)
+
     puts response.status_code
   end
 end
--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -2,13 +2,23 @@ class CommentMailer < ApplicationMailer
   default from: 'hirocueki@gmail.com'
 
   def commented(comment)
-    @comment = comment
+    #@comment = comment
+    #
+    #unless comment.post.writer?(comment.user)
+    #  mail(
+    #      subject: "#{comment.user.name}さんからコメントがつきました！",
+    #      to: comment.post.user.email
+    #  )
+    #end
 
-    unless comment.post.writer?(comment.user)
-      mail(
-          subject: "#{comment.user.name}さんからコメントがつきました！",
-          to: comment.post.user.email
-      )
-    end
+    @comment = comment
+    mail(subject: "#{comment.user.name}さんからコメントがつきました！", to: comment.post.user.email)
+  end
+
+  def hello
+    mail(
+        subject: "テストメール",
+        to: "hirocueki@gmail.com"
+    )
   end
 end
