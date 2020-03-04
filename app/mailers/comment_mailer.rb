d872d931ee1fe4d1773ee7240ce9253c99a9fc62
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-05 06:11:53 +0900

    自分の投稿に対しての自分のコメントは通知させない

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -2,23 +2,13 @@ class CommentMailer < ApplicationMailer
   default from: 'hirocueki@gmail.com'
 
   def commented(comment)
-    #@comment = comment
-    #
-    #unless comment.post.writer?(comment.user)
-    #  mail(
-    #      subject: "#{comment.user.name}さんからコメントがつきました！",
-    #      to: comment.post.user.email
-    #  )
-    #end
-
     @comment = comment
-    mail(subject: "#{comment.user.name}さんからコメントがつきました！", to: comment.post.user.email)
-  end
 
-  def hello
-    mail(
-        subject: "テストメール",
-        to: "hirocueki@gmail.com"
-    )
+    unless comment.post.writer?(comment.user)
+      mail(
+          subject: "#{comment.user.name}さんからコメントがつきました！",
+          to: comment.post.user.email
+      )
+    end
   end
 end
