932e611d2ff0d2981aca2fe0939ea062b27e1a79
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-05 06:16:06 +0900

    通知する内容（投稿）があるときのみメールする

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -4,9 +4,11 @@ class PostMailer < ApplicationMailer
   def likes_ranking(users, posts)
     @posts = posts
 
-    mail(
-      subject: "「いいね」デイリーランキング",
-      to: users.pluck(:email)
-    )
+    if @post.present?
+      mail(
+        subject: "「いいね」デイリーランキング",
+        to: users.pluck(:email)
+      )
+    end
   end
 end
