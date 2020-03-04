24622071bce6af23dbf815748260f9df38dfc9f1
Hiroshi Ueki <hirocueki@gmail.com>
2020-03-04 22:53:53 +0900

    いいね数デイリーランキング通知メールの、集計ロジックの修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,6 @@ class Like < ApplicationRecord
   belongs_to :post, counter_cache: true
 
   scope :yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :more_than_likes_posts, -> { group(:post_id)
+                                         .order(count: :desc) }
 end
